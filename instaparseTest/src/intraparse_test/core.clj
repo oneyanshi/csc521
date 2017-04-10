ebbfac93004df9fd4feaea74cad27a635dca76a1
Yan Shi <ys7615a@student.american.edu>
2017-04-10 15:49:02 -0400

    Update intsa

---
+++ instaparseTest/src/intraparse_test/core.clj
@@ -0,0 +1,14 @@
+(ns instaparse_test.core
+  (:require [instaparse.core :as insta]))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(def as-and-bs
+  (insta/parser
+    "S = AB*
+     AB = A B 
+     A = 'a' + 
+     B = 'b' +" ))

a3366e927bf6d2979c4305ce9899cbd042d2a30c
Yan Shi <ys7615a@student.american.edu>
2017-04-21 11:31:42 -0400

    add clojure-quirk

---
+++ clojure-quirk/src/clojure_quirk/core.clj
@@ -0,0 +1,51 @@
+(ns clojure-quirk.core
+  (:require [instaparse.core :as insta]))
+
+; the goal is to do lein run -pt < simple.q 
+; the goal is to do lein run < simple.q 
+; sample for reading all stdin 
+; (def stdin (slurp *in*)) 
+
+; begin utility functions 
+(defn third [aList] (nth aList 2))
+(defn fourth [aList] (nth aList 3))
+(defn fifth [aList] (nth aList 4)) 
+(defn sixth [aList] (nth aList 5))
+
+(defn CallbyLabel [funLabel & args]
+  (apply(resolve(symbol(name funLabel))) args))
+ 
+; end utility functions 
+
+
+; breakdown 
+
+
+; end breakdown 
+
+; our cool parser 
+ (def parser 
+  (insta/parser (slurp "resources/quirk-ebnf-grammar.ebnf")
+    :auto-whitespace :standard)
+  )
+ 
+(defn -main [& args]
+ ;(println(first *command-line-args*))
+ 
+ ; takes in input
+ (def stdin (slurp *in*))
+ 
+ ; is there a command for -pt? 
+ (if (.equals"-pt"(first *command-line-args*))
+   (def SHOW_PARSE_TREE true)
+   )
+ 
+ (def parse-tree(parser stdin))
+ 
+ (if(= true SHOW_PARSE_TREE)
+   (println parse-tree))
+)
+
+;(println(parser "var x = (5 * 2) / 5 print x"))
+
+

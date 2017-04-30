ba68fd71be9c168fc21467c2fc63c98229aac50f
Yan Shi <ys7615a@student.american.edu>
2017-04-30 14:07:54 -0400

    added in comments

--- clojure-quirk/src/clojure_quirk/core.clj
+++ clojure-quirk/src/clojure_quirk/core.clj
@@ -18,8 +18,91 @@
  
 ; end utility functions 
 
+; <Program> --> <Statement> <Program> | <Statement> 
+(defn Program [subtree scope]
+  (cond
+    (= :Program (first(third subtree)))
+    ((CallByLabel (first(second subtree)) (second subtree) (scope)
+                  (CallByLabel (first(third subtree)) (third subtree) scope))
+      :else
+      (CallByLabel (first(second subtree)) (second subtree) scope)
+      )
+    )
+  )
 
-; breakdown 
+; <Statement> -> <FunctionDeclaration> | <Assignment> | <Print> 
+
+
+; <FunctionDeclaration> -> FUNCTION <Name> PAREN <FunctionParams> LBRACE <FunctionBody> RBRACE
+
+
+; <FunctionParams> -> <NameList> RPAREN | RPAREN 
+
+; <FunctionBody> -> <Program> <Return> | <Return> 
+
+; <Return> -> RETURN <ParameterList> 
+
+; <Assignment> - <SingleAssignment> | <MultipleAssignment> 
+
+; <SingleAssignment> -> VAR <Name> ASSIGN <Expression> 
+
+; <MultipleAssignment> -> VAR <NameList> ASSIGN <FunctionCall>
+
+; <Print> -> PRINT <Expression> 
+
+; <NameList> -> <Name> COMMA <NameList> | <Name> 
+
+; <ParameterList> -> <Parameter COMMA <ParameterList> | <Parameter> 
+
+; <Parameter> -> <Expression> | <Name> 
+
+; <Expression> -> <Term> ADD <Expression> | <Term> SUB <Expression> | <Term> 
+
+; <Term> -> <Factor> MULT <Term> | <Factor> DIV <Term> | <Factor> 
+
+; <Factor> -> <SubExpression> EXP <Factor> | <SubExpression> | <FunctionCall> | <Value> EXP <Factor> | <Value> 
+
+; <FunctionCall> -> #<FunctionCall> ->  <Name> LPAREN <FunctionCallParams> COLON <Number> | <Name> LPAREN <FunctionCallParams>
+
+; <FunctionCallParams> ->  <ParameterList> RPAREN | RPAREN
+
+
+; <SubExpression> -> LPAREN <Expression> RPAREN
+
+; <Value> -> <Name> | <Number>
+
+; <Name> -> IDENT | SUB IDENT | ADD IDENT
+
+; <Number> -> NUMBER | SUB NUMBER | ADD NUMBER
+
+
+(defn Number [subtree scope]
+  (cond
+    (= :Number(
+      ) 
+    )
+  
+    )
+  )
+
+; returns a map with local variable bindings when calling a function 
+; needs to have a list of values 
+(def VarBindLoop [paramList valueList] 
+  (if(= 1 (count paramList))
+    (assoc {} scope (first paramList) (first valueLIst))
+    (merge ( assoc {} (first paramlist) (first valuelist)) 
+      (VarBindLoop 
+        (rest paramList) 
+        (rest valueList) 
+        )
+      )
+    )
+ )
+   
+
+
+; function for interpretation 
+(defn interpret-quirk [subtree scope] (CallByLabel (first subtree) subtree {}))
 
 
 ; end breakdown 
@@ -29,7 +112,10 @@
   (insta/parser (slurp "resources/quirk-ebnf-grammar.ebnf")
     :auto-whitespace :standard)
   )
- 
+
+
+
+; this is our main  
 (defn -main [& args]
  ;(println(first *command-line-args*))
  

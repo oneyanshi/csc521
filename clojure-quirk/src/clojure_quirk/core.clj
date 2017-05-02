6033ef002432d7c8afab2768a787314d4143131c
Yan Shi <ys7615a@student.american.edu>
2017-05-01 20:07:34 -0400

    Updated parse tree

--- clojure-quirk/src/clojure_quirk/core.clj
+++ clojure-quirk/src/clojure_quirk/core.clj
@@ -13,28 +13,32 @@
 (defn sixth [aList] (nth aList 5))
 
 ; calling the functions by the label :Statement
-(defn CallbyLabel [funLabel & args]
-  (apply(resolve(symbol(name funLabel))) args))
+(defn CallBylabel [funLabel & args]
+  (apply(ns-resolve 'clojure-quirk.core (symbol(name funLabel))) args)
+  )
  
-; end utility functions 
 
 ; <Program> --> <Statement> <Program> | <Statement> 
-(defn Program [subtree scope]
-  (cond
-    (= :Program (first(third subtree)))
-    ((CallByLabel (first(second subtree)) (second subtree) (scope)
-                  (CallByLabel (first(third subtree)) (third subtree) scope))
-      :else
-      (CallByLabel (first(second subtree)) (second subtree) scope)
-      )
+(defn Program [subtree scope] 
+  (cond 
+    ; Program0 
+    (= :Program (first (third subtree))) 
+    (CallByLabel (first (second subtree)) (second subtree) scope 
+      (CallByLabel(first (third subtree)) (third subtree) scope)) 
+    
+    ; Program1
+    :else 
+    (CallByLabel (first (second subtree)) (second subtree) scope) 
     )
   )
 
 ; <Statement> -> <FunctionDeclaration> | <Assignment> | <Print> 
+(defn Statement [subtree scope] 
+  (CallByLabel (first (second subtree)) (second subtree) scope)
+  )
 
 
 ; <FunctionDeclaration> -> FUNCTION <Name> PAREN <FunctionParams> LBRACE <FunctionBody> RBRACE
-
 
 ; <FunctionParams> -> <NameList> RPAREN | RPAREN 
 
@@ -43,64 +47,89 @@
 ; <Return> -> RETURN <ParameterList> 
 
 ; <Assignment> - <SingleAssignment> | <MultipleAssignment> 
+(defn Assignment [subtree scope] 
+  (CallByLabel (first (second subtree)) (second subtree) scope)
+  
+  )
 
 ; <SingleAssignment> -> VAR <Name> ASSIGN <Expression> 
 
 ; <MultipleAssignment> -> VAR <NameList> ASSIGN <FunctionCall>
 
 ; <Print> -> PRINT <Expression> 
+(defn Print [subtree scope]
+  (println(CallByLabel (first (second subtree)) (second subtree) scope))
+  )
 
 ; <NameList> -> <Name> COMMA <NameList> | <Name> 
 
 ; <ParameterList> -> <Parameter COMMA <ParameterList> | <Parameter> 
 
 ; <Parameter> -> <Expression> | <Name> 
+(defn Parameter [subtree scope] 
+  (CallByLabel (first (second subtree)) (second subtree) scope)
+  )
+
 
 ; <Expression> -> <Term> ADD <Expression> | <Term> SUB <Expression> | <Term> 
+(defn Expression [subtree scope] 
+	(cond
+   (= 2 (count subtree))
+   (CallByLabel (first (second subtree)) (second subtree) scope) 
+   (= :Term (first (second subtree)))
+		((if (.equals :ADD (first (third subtree)))
+			(+ (CallByLabel (first (second subtree))(second subtree) scope)
+			(CallByLabel (first (fourth subtree))(fourth subtree) scope)))
+		(if (.equals :SUB (first (third subtree)))
+			(- (CallByLabel (first (second subtree))(second subtree) scope)
+			(CallByLabel (first (fourth subtree))(fourth subtree) scope))))	
+	:else
+	(CallByLabel (first (second subtree)) (second subtree) scope)
+ )
+)
 
 ; <Term> -> <Factor> MULT <Term> | <Factor> DIV <Term> | <Factor> 
+(defn Term [subtree scope] 
+  (cond 
+    (= 2 (count subtree))
+    (CallByLabel (first (second subtree)) (second subtree) scope) 
+    (= :Factor (first (second subtree)))
+    ((if (.equals :MULT (first (third subtree)))
+       (* (CallByLabel (first (second subtree))(second subtree) scope)
+          (CallByLabel (first (fourth subtree))(fourth subtree) scope)))
+     (if(.equals :DIV (first (third subtree)))
+       (- (CallByLabel (first(second subtree))(second subtree) scope)
+          (CallByLabel (first (fourth subtree))(fourth subtree) scope))))   
+    :else 
+    (CallByLabel (first (second subtree)) (second subtree) scope)
+    )
+  ) 
 
 ; <Factor> -> <SubExpression> EXP <Factor> | <SubExpression> | <FunctionCall> | <Value> EXP <Factor> | <Value> 
 
+
 ; <FunctionCall> -> #<FunctionCall> ->  <Name> LPAREN <FunctionCallParams> COLON <Number> | <Name> LPAREN <FunctionCallParams>
+
 
 ; <FunctionCallParams> ->  <ParameterList> RPAREN | RPAREN
 
 
 ; <SubExpression> -> LPAREN <Expression> RPAREN
 
+
 ; <Value> -> <Name> | <Number>
+(defn Value [subtree scope] 
+  (CallByLabel (first (second subtree)) (second subtree) scope) 
+  )
+
 
 ; <Name> -> IDENT | SUB IDENT | ADD IDENT
+
 
 ; <Number> -> NUMBER | SUB NUMBER | ADD NUMBER
 
 
-(defn Number [subtree scope]
-  (cond
-    (= :Number(
-      ) 
-    )
-  
-    )
-  )
-
-; returns a map with local variable bindings when calling a function 
-; needs to have a list of values 
-(def VarBindLoop [paramList valueList] 
-  (if(= 1 (count paramList))
-    (assoc {} scope (first paramList) (first valueLIst))
-    (merge ( assoc {} (first paramlist) (first valuelist)) 
-      (VarBindLoop 
-        (rest paramList) 
-        (rest valueList) 
-        )
-      )
-    )
- )
    
-
-
 ; function for interpretation 
 (defn interpret-quirk [subtree scope] (CallByLabel (first subtree) subtree {}))
 
@@ -113,12 +142,9 @@
     :auto-whitespace :standard)
   )
 
-
-
 ; this is our main  
 (defn -main [& args]
- ;(println(first *command-line-args*))
- 
+
  ; takes in input
  (def stdin (slurp *in*))
  
@@ -131,9 +157,10 @@
  
  ; if SHOW_PARSE_TREE = true, then print the parse tree
  (if(= true SHOW_PARSE_TREE)
-   (println parse-tree))
-)
+   (println parse-tree)
+   ;(interpret-quirk parse-tree {}))
+   )
+ ) 
 
-;(println(parser "var x = (5 * 2) / 5 print x"))
 
 

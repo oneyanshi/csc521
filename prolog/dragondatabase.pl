9fbabd97be409d2ca0ef54c0539d06f80469c5da
Yan Shi <ys7615a@student.american.edu>
2017-05-07 19:17:21 -0400

    updated rules

--- prolog/dragondatabase.pl
+++ prolog/dragondatabase.pl
@@ -10,23 +10,23 @@ dragon. fae. salamander. wyrm. quetzakcoatl.
 amphithere. lindworm. lungDragon. drake. hydra.
 wyvern. cockatrice. kirin. seaSerpent.
 
-
-
 isEurodragon(dragon).
 isEurodragon(fae).
+
 isDraganid(salamander).
 isDraganid(X) :- eurodragon(X).
 
 isDrakoid(wyvern).
 isDrakoid(drake).
 isDrakoid(hydra).
-
 isDrakid(X) :- serpentoid(X); drakoid(X).
 
 isTrueSerpent(wyvern).
 isTrueSerpent(quetzakcoatl).
+
 isBrachioserpent(amphithere).
 isBrachioserpent(lindworm).
+
 isSerpentoid(lungDragon).
 isSerpentoid(X) :- trueSerpent(X); brachioserpent(X).
 
@@ -55,6 +55,21 @@ hasAppendagesLost(X) :- trueSerpent(X) ; brachioserpent(X).
 
 hasExtraAppendages(X) :- draganid(X).
 
-hasSizeReduct(fae).
+hasSizeReduction(fae).
 
-dragon(eurodragon, wings).
+dragon(draganid, eurodragon, fourLegs, wings).
+fae(draganid, eurodragon, fourLegs, sizeReduction).
+salamander(draganid, elongatedBody, fourLegs, extraAppendages).
+wyvern(drakid, drakoid, fourLegs, wings).
+cockatrice(bird, feathers, fourLegs, wings).
+
+lungdragon(serpentoid, fourLegs, elongatedBody).
+drake(drakid, fourLegs, drakoid).
+hydra(drakid, fourLegs, drakoid).
+kirin(mammal, fourLegs, fur).
+seaSerpent(rayFinnedFish, elongatedBody).
+
+wyrm(serpentoid, trueSerpent, fourLegs, elongatedBody, appendagesLost).
+quetzakcoatl(serpentoid, trueSerpent, fourLegs, elongatedBody, appendagesLost, feathers).
+amphithere(serpentoid, brachioserpent, fourLegs, elongatedBody, wings, appendagesLost).
+lindworm(serpentoid, brachioserpent, fourLegs, elongatedBody, appendagesLost).
